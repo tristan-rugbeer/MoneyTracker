54bf4f3af2d24793fcb1a5dd91e30804ef934db8
tristan-rugbeer <tristanrugbeer93@gmail.com>
2024-06-09 13:41:56 +0200

    Added Frontend Files

---
+++ MoneyTracker/src/App.jsx
@@ -0,0 +1,85 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [name, setName] = useState('');
+  const [description, setDescription] = useState('');
+  const [dateTime, setDateTime] = useState('');
+
+  return (
+    <main>
+      <h1>$400<span>.00</span></h1>
+
+      <form>
+        <div className='basic'>
+          <input type="text"
+                 placeholder={'+200$ Samm'}
+                 value={name}
+                 onChange={ev => setName(ev.target.value)}
+          />
+          
+          <input type="datetime-local"
+                 value={dateTime}
+                 onChange={ev => setDateTime(ev.target.value)}
+          />
+        </div>
+
+        <div className='description'>
+          <input type='text' 
+                placeholder={'description'}
+                value={description}
+                onChange={ev => setDescription(ev.target.value)}
+          />
+        </div>
+        <button type='submit'>Add new Transaction</button>
+      </form>
+
+      <div className='transactions'>
+
+        <div className='transaction'>
+          <div className="left">
+            <div className="name">TV</div>
+            <div className="description">This is a TV</div>
+          </div>
+
+          <div className="right">
+            <div className="price red">$-20</div>
+            <div className="datetime">2024-06-05 21:50</div>
+          </div>
+        </div>
+
+        <div className='transaction'>
+          <div className="left">
+            <div className="name">Salary</div>
+            <div className="description">You got paid</div>
+          </div>
+
+          <div className="right">
+            <div className="price green">$200</div>
+            <div className="datetime">2024-06-05 21:50</div>
+          </div>
+
+        </div>
+
+        <div className='transaction'>
+          <div className="left">
+            <div className="name">TV</div>
+            <div className="description">This is a TV</div>
+          </div>
+
+          <div className="right">
+            <div className="price">$20</div>
+            <div className="datetime">2024-06-05 21:50</div>
+          </div>
+
+        </div>
+
+      </div>
+
+    </main>
+  )
+}
+
+export default App
